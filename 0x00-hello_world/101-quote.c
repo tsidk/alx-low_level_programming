d1a6c977985b1c06b47a661936b179753b011575
tsidk <tsidk29@gmail.com>
2021-06-21 01:38:27 -0700

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 /**
-* main - main block
-* Return: 1
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-write(2,
-"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-s);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
