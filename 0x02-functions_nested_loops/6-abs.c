969af191dc590e95983b6eb4eed1dd4db28790e4
tsidk <tsidk29@gmail.com>
2021-06-23 19:01:45 -0700

    nested_loops_functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+int print_last_digit(int n)
+{
+int nv;
+
+if (n < 0)
+{
+nv = -1 * (n % 10);
+_putchar(nv + '0');
+return (nv);
+}
+else
+{
+nv = n % 10;
+_putchar(nv + '0');
+return (nv);
+}
+}
